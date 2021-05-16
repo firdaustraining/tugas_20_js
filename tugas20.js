b8adb76a6ff95296a0413cf64da7c8615c9fd564
Faza <firdausauliafaza@gmail.com>
2021-05-16 18:08:40 +0700

    niomic.com

---
+++ tugas20.js
@@ -0,0 +1,8 @@
+function carikata(){
+  let lorem = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable."
+  console.log(lorem.match("are"))
+  console.log(lorem.match("words"))
+
+}
+
+carikata();
